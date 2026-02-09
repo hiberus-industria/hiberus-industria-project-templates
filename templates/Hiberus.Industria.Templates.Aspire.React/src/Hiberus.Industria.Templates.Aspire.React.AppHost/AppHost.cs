2ffd889b12be2d79cdd6742e331a6044f52570f3
jblascoisazac <jblascoi@hiberus.com>
2026-02-09 13:57:05 +0100

    feat: archivos base linting + formatting dotnet

--- templates/Hiberus.Industria.Templates.Aspire.React/src/Hiberus.Industria.Templates.Aspire.React.AppHost/AppHost.cs
+++ templates/Hiberus.Industria.Templates.Aspire.React/src/Hiberus.Industria.Templates.Aspire.React.AppHost/AppHost.cs
@@ -1,10 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var server = builder.AddProject<Projects.Hiberus_Industria_Templates_Aspire_React_Server>("server")
+var server = builder
+    .AddProject<Projects.Hiberus_Industria_Templates_Aspire_React_Server>("server")
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
-var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
+var webfrontend = builder
+    .AddViteApp("webfrontend", "../frontend")
     .WithReference(server)
     .WaitFor(server);
 
