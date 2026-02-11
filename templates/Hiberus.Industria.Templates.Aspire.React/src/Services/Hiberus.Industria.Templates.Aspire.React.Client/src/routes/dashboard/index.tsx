70fc2987bb53b408cb07387114bd024948fd9154
jblascoisazac <jblascoi@hiberus.com>
2026-02-11 19:58:57 +0100

    fix: márgenes ruta dashboard

--- templates/Hiberus.Industria.Templates.Aspire.React/src/Services/Hiberus.Industria.Templates.Aspire.React.Client/src/routes/dashboard/index.tsx
+++ templates/Hiberus.Industria.Templates.Aspire.React/src/Services/Hiberus.Industria.Templates.Aspire.React.Client/src/routes/dashboard/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/(dashboard)/dashboard/")({
 function RouteComponent() {
     return (
         <main className="flex flex-1 flex-col">
-            <div className="flex flex-1 flex-col gap-4 px-4">
+            <div className="flex flex-1 flex-col gap-4 p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="bg-muted/50 aspect-video rounded-xl" />
                     <div className="bg-muted/50 aspect-video rounded-xl" />
